75a09a66b74d5ff9270c2fc743ac91a5670c3f3a
Duy <qduyhoang@gmail.com>
2018-07-03 10:53:05 -0400

    Use requests library instead of urllib for better support. Add progress bar for downloading

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import xml.etree.ElementTree as ET
 import json
 import re
-import urllib.request
+import requests
 import io
 import gzip
 import sys
@@ -9,45 +9,36 @@ import shutil
 import os
 import subprocess
 from tqdm import tqdm
+import math
 
 
 def retrieveData(baseURL = "https://dumps.wikimedia.org/enwiki/latest/"):
 	file_path = 'data/'
-	chunk_size = 4096
-	#Get html content
-	html_page = urllib.request.urlopen(baseURL)
-	#Convert html content to string and read
-	page = html_page.read().decode('utf-8') 
+	#Get html content and convert to string
+	html_page = requests.get(baseURL).text
 	#Get history files'name
 	pattern = re.compile('enwiki-latest-abstract11.xml.gz')  #Replace this small file for testing purpose
-	result = pattern.findall(page)											  #enwiki-latest-abstract11.xml.gz  
+	result = pattern.findall(html_page)											  #enwiki-latest-abstract11.xml.gz  
 	#Filter overlapping results												  #original: 'enwiki-latest-stub-meta-history[0-9]{0,3}.xml.gz'
 	file_names = set(result)
 	for file_name in file_names:
 		if file_name == 'enwiki-latest-abstract11.xml.gz':
-			out_file_path = file_name[:-3]
+			#Download gzip file
+			response = requests.get(baseURL + file_name, stream = True)
 			with gzip.open(file_path+file_name, 'wb') as compressed_file:
-				#Download gzip file
-				response = urllib.request.urlopen(baseURL + file_name)
-				#Save file
-				shutil.copyfileobj(response, compressed_file)
-				print("Downloading %s" % file_name)
+				# Total size in bytes.
+				file_size = int(response.headers.get('content-length', 0));
+				block_size = 4096
 				bytes_so_far = 0
-				file_size = response.info().get_all('Content-Length')[0]
-				while True:
-					chunk = response.read(chunk_size)
-					bytes_so_far += len(chunk)
-					#progress bar
-					done = int(50 * bytes_so_far / int(file_size))
-					sys.stdout.write("\r[%s%s]" % ('=' * done, ' ' * (50-done)) )    
-					sys.stdout.flush()
-					if not chunk:
-						sys.stdout.write("==========Decompressing===========")    
-						sys.stdout.flush()
-						break
+				#Write gzip response to a new file and show progress bar
+				for data in tqdm(response.iter_content(block_size), total=math.ceil(file_size//block_size) , unit='KB', unit_scale=True):
+					bytes_so_far += len(data)
+					compressed_file.write(data)
+
 			#Decompress file and save
 			with gzip.open(file_path+file_name) as compressed_file:
 				file_content = gzip.decompress(compressed_file.read())
+				out_file_path = file_name[:-3]
 				with open(file_path+out_file_path, 'wb') as out_file:
 					out_file.write(file_content)
 			#Delete compressed file after done
